94f381815bb9544020c1221f8952cf37344cda91
Patryk Szczygieł <48336680+deestroyeer123@users.noreply.github.com>
2020-06-07 02:41:40 +0200

    better adding orders

--- Restauracja-Aplikacja/app/src/main/java/com/example/restauracja/DeliveryActivity.java
+++ Restauracja-Aplikacja/app/src/main/java/com/example/restauracja/DeliveryActivity.java
@@ -48,18 +48,20 @@ import static com.example.restauracja.MainActivity.name_meal;
 public class DeliveryActivity extends AppCompatActivity {
 
     DatabaseHelper db;
-    TableRow card_line1, card_line2, card_line3;
+    TableRow card_line1, card_line2, card_line3, line_clear;
     Toolbar toolbar;
     Spinner spin1, spin2, spin3, spin4, spin5, spin6, spin7, spin8;
     EditText del_starters, del_soups, del_salads, del_pasta, del_meats, del_fishes, del_desserts, del_pizza;
     EditText del_street, del_number, del_code, del_city, del_name, del_surname, del_phone, del_mail, del_inf;
     EditText del_card_name, del_card_number, del_card_surname, del_card_date, del_card_ccv;
     RadioButton del_card, del_cash, del_online;
-    Button btn_enter;
+    Button btn_enter, del_order_clear;
+    Button res_add_starters, res_add_soups, res_add_salads, res_add_pasta, res_add_meats, res_add_fishes, res_add_desserts, res_add_pizza;
     RadioGroup del_radio_group;
-    TextView del_order;
+    TextView del_order, tv_orders;
     String[] parsedDistance = new String[1];
     String[] parsedDuration = new String[1];
+    int sum = 0;
     private String MyLocation = "Wroclaw, Szewska, 27";
 
     @SuppressLint("WrongViewCast")
@@ -73,6 +75,18 @@ public class DeliveryActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.app_bar);
         toolbar.setTitle(R.string.delivery);
         setSupportActionBar(toolbar);
+
+        line_clear = findViewById(R.id.line_clear);
+        del_order_clear = findViewById(R.id.del_order_clear);
+        tv_orders = findViewById(R.id.tv_orders);
+        res_add_starters = findViewById(R.id.res_add_starters);
+        res_add_soups = findViewById(R.id.res_add_soups);
+        res_add_salads = findViewById(R.id.res_add_salads);
+        res_add_pasta = findViewById(R.id.res_add_pasta);
+        res_add_meats = findViewById(R.id.res_add_meats);
+        res_add_fishes = findViewById(R.id.res_add_fishes);
+        res_add_desserts = findViewById(R.id.res_add_desserts);
+        res_add_pizza = findViewById(R.id.res_add_pizza);
 
         spin1 = findViewById(R.id.spinner1);
         spin2 = findViewById(R.id.spinner2);
@@ -115,13 +129,76 @@ public class DeliveryActivity extends AppCompatActivity {
         del_radio_group = findViewById(R.id.del_radio_group);
         del_order = findViewById(R.id.del_order);
 
-
         final Spinner[] tab_spinners = {spin1, spin2, spin3, spin4, spin5, spin6, spin7, spin8};
         final String[] tab_meals = {getString(R.string.menu_starters), getString(R.string.menu_soups), getString(R.string.menu_salads),
                 getString(R.string.menu_pasta), getString(R.string.menu_meats), getString(R.string.menu_fishes), getString(R.string.menu_desserts),
                 getString(R.string.menu_pizza)};
         final EditText[] tab_edits = {del_starters, del_soups, del_salads, del_pasta, del_meats, del_fishes, del_desserts, del_pizza};
+        final Button[] tab_buttons = {res_add_starters, res_add_soups, res_add_salads, res_add_pasta, res_add_meats,
+                res_add_fishes, res_add_desserts, res_add_pizza};
         loading_spinners(tab_meals, tab_spinners);
+
+
+        del_order_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TableLayout.LayoutParams tableRowParams =
+                        new TableLayout.LayoutParams(0,0);
+                tableRowParams.setMargins(0,0,0,0);
+                line_clear.setLayoutParams(tableRowParams);
+                tv_orders.setText("-");
+                sum = 0;
+                line_clear.setVisibility(View.INVISIBLE);
+            }
+        });
+        res_add_starters.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_order(tab_spinners, tab_edits, tab_buttons, tab_meals);
+            }
+        });
+        res_add_soups.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_order(tab_spinners, tab_edits, tab_buttons, tab_meals);
+            }
+        });
+        res_add_salads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_order(tab_spinners, tab_edits, tab_buttons, tab_meals);
+            }
+        });
+        res_add_pasta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_order(tab_spinners, tab_edits, tab_buttons, tab_meals);
+            }
+        });
+        res_add_meats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_order(tab_spinners, tab_edits, tab_buttons, tab_meals);
+            }
+        });
+        res_add_fishes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_order(tab_spinners, tab_edits, tab_buttons, tab_meals);
+            }
+        });
+        res_add_desserts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_order(tab_spinners, tab_edits, tab_buttons, tab_meals);
+            }
+        });
+        res_add_pizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                add_order(tab_spinners, tab_edits, tab_buttons, tab_meals);
+            }
+        });
 
         del_online.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -176,7 +253,7 @@ public class DeliveryActivity extends AppCompatActivity {
                 int add_sec;
                 int selectedRadioButtonID = del_radio_group.getCheckedRadioButtonId();
                 RadioButton selectedRadioButton = findViewById(selectedRadioButtonID);
-                if(check(tab_spinners, tab_edits))
+                if(check())
                 {
                     add_sec = 300 * db.count_orders();
                     getDistance(MyLocation, del_city.getText().toString() + "," + del_street.getText().toString());
@@ -186,26 +263,19 @@ public class DeliveryActivity extends AppCompatActivity {
                         String wTime = DateUtils.formatElapsedTime(lTime_sec);
                         Calendar time_realization = Calendar.getInstance();
                         time_realization.add(Calendar.SECOND, lTime_sec);
-                        int kwota = 0;
-                        StringBuilder order = new StringBuilder();
-                        for (int i = 0; i < tab_spinners.length; i++)
-                            if (tab_spinners[i].getSelectedItemPosition() != 0) {
-                                order.append(tab_spinners[i].getSelectedItem().toString()).append(" x").append(tab_edits[i].getText().toString()).append("\n");
-                                kwota += (db.find_price(tab_spinners[i].getSelectedItem().toString(), tab_meals[i]) * Integer.parseInt(tab_edits[i].getText().toString()));
-                            }
                         if (selectedRadioButton.getText().toString().equals("Online"))
                             isCorrect = db.set_order(del_name.getText().toString(),
                                     del_surname.getText().toString(), del_phone.getText().toString(), del_mail.getText().toString(),
                                     del_street.getText().toString(), del_number.getText().toString(), del_code.getText().toString(),
                                     del_city.getText().toString(), del_inf.getText().toString(), selectedRadioButton.getText().toString(),
-                                    order.toString(), String.valueOf(time_realization.getTimeInMillis()),
+                                    tv_orders.getText().toString(), String.valueOf(time_realization.getTimeInMillis()),
                                     del_card_name.getText().toString() + " " + del_card_surname.getText().toString(), del_card_number.getText().toString(),
                                     del_card_date.getText().toString(), del_card_ccv.getText().toString());
                         else isCorrect = db.set_order(del_name.getText().toString(),
                                 del_surname.getText().toString(), del_phone.getText().toString(), del_mail.getText().toString(),
                                 del_street.getText().toString(), del_number.getText().toString(), del_code.getText().toString(),
                                 del_city.getText().toString(), del_inf.getText().toString(), selectedRadioButton.getText().toString(),
-                                order.toString(), String.valueOf(time_realization.getTimeInMillis()), "-", "-", "-", "-");
+                                tv_orders.getText().toString(), String.valueOf(time_realization.getTimeInMillis()), "-", "-", "-", "-");
                         if (isCorrect) {
                             Toast.makeText(DeliveryActivity.this, "Dodano do bazy danych", Toast.LENGTH_LONG).show();
                         } else {
@@ -214,14 +284,14 @@ public class DeliveryActivity extends AppCompatActivity {
 
                         if (isCorrect) {
 
-                            String buff = getString(R.string.del_order) + "\n" + order.toString() + "\n" +
+                            String buff = getString(R.string.del_order) + "\n" + tv_orders.getText().toString() + "\n" +
                                     getString(R.string.res_name) + ": " + del_name.getText().toString() + "\n" +
                                     getString(R.string.res_surname) + ": " + del_surname.getText().toString() + "\n" +
                                     getString(R.string.res_phone) + ": " + del_phone.getText().toString() + "\n" +
                                     getString(R.string.address) + del_street.getText().toString() + " " + del_number.getText().toString() + " " + del_city.getText().toString() + "\n" +
                                     getString(R.string.payment) + selectedRadioButton.getText().toString() + "\n" +
                                     getString(R.string.res_information) + ": " + (TextUtils.isEmpty(del_inf.getText()) ? "-" : del_inf.getText().toString()) + "\n" +
-                                    "\n" + getString(R.string.sum) + kwota + " zł" +
+                                    "\n" + getString(R.string.sum) + sum + " zł" +
                                     "\n" + getString(R.string.wTime) + wTime;
                             show_messege(getString(R.string.del_mess_title), getString(R.string.del_mess_body) + "\n\n" + buff);
                         } else {
@@ -371,12 +441,12 @@ public class DeliveryActivity extends AppCompatActivity {
         return true;
     }
 
-    public boolean check(Spinner[] tab_spinners, EditText[] tab_edits)
+    public boolean check()
     {
         int j = 0;
         int selectedRadioButtonID = del_radio_group.getCheckedRadioButtonId();
         RadioButton selectedRadioButton = findViewById(selectedRadioButtonID);
-        if(!isSelected(tab_spinners, tab_edits)) del_order.setError("Wybierz jedzenie oraz podaj ilość!");
+        if(tv_orders.getText().toString().equals("-")) tv_orders.setError("Wybierz jedzenie");
         else j++;
         if(TextUtils.isEmpty(del_name.getText())) del_name.setError("Imię jest wymagane!");
         else j++;
@@ -477,6 +547,33 @@ public class DeliveryActivity extends AppCompatActivity {
         getBaseContext().getResources().updateConfiguration(config,
                 getBaseContext().getResources().getDisplayMetrics());
         DeliveryActivity.this.setContentView(R.layout.activity_delivery);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void add_order (Spinner[] tab_spinners, EditText[] tab_edits, Button[] tab_buttons, String[] tab_meals)
+    {
+        if(!isSelected(tab_spinners, tab_edits)) del_order.setError("Wybierz jedzenie oraz podaj ilość!");
+        else {
+            TableLayout.LayoutParams tableRowParams =
+                    new TableLayout.LayoutParams(50,100);
+            tableRowParams.setMargins(0,0,0,50);
+            line_clear.setLayoutParams(tableRowParams);
+            line_clear.setVisibility(View.VISIBLE);
+            String orders_prev;
+            if (tv_orders.getText().toString().equals("-")) orders_prev = "";
+            else orders_prev = tv_orders.getText().toString();
+            int num = 0;
+            for (int i = 0; i < tab_buttons.length; i++) {
+                if (tab_buttons[i].isPressed()) {
+                    num = i;
+                    break;
+                }
+            }
+            sum += (db.find_price(tab_spinners[num].getSelectedItem().toString(), tab_meals[num]) * Integer.parseInt(tab_edits[num].getText().toString()));
+            tv_orders.setText(orders_prev + "+ " + tab_spinners[num].getSelectedItem().toString() + " x" + tab_edits[num].getText().toString() + "\n");
+            tab_spinners[num].setSelection(0);
+            tab_edits[num].setText(null);
+        }
     }
 
 }
